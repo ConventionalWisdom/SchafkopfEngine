700ec68157953ff261452edaea2940ffd2991f22
Nicolas Lehment <Lehment@tum.de>
2020-03-21 12:00:59 +0100

    weitere Basteleien...

--- schafkopfEngine.py
+++ schafkopfEngine.py
@@ -1,30 +1,45 @@
 import pandas as pd
 import numpy as np
 
-def createCards():
-    farben = ['Eiche', 'Gras', 'Herz', 'Schelle']
-    Werte = {'Ober' : 3, 'Unter' : 2, 'Ass' : 11, '10' : 10, 'König' : 4}
-    for i in range(9,6,-1):
-        Werte[str(i)] = 0
-    karten = pd.DataFrame([{'Farbe' : f, 'Name': n, 'Wert' : w} for f in farben for n,w in Werte.items()])
+#globale definitionen
+farben = {'Eiche':0, 'Gras':1, 'Herz':2, 'Schelle':3}
+
+def erstelleBlatt(blattLang):
+    if blattLang == 'lang':
+        niedrigste = 7
+    else:
+        niedrigste = 8
+    werte = {'Ober' : 3, 'Unter' : 2, 'Ass' : 11, '10' : 10, 'König' : 4}
+    rang = {'Ober' : 0, 'Unter' : 1, 'Ass' : 2, '10' : 3, 'König' : 4}
+    for i,p in enumerate(range(9,niedrigste-1,-1)):
+        werte[str(p)] = 0
+        rang[str(p)] = i+5
+    karten = pd.DataFrame([{'Farbe' : f, 'Name': nw, 'Wert' : w, 'Basis Rang' : r} for f,order in farben.items() for (nw, w),(nr, r) in zip(werte.items(),rang.items())])
     print(karten)
     return karten
+
+def verteileKarten(karten):
+    #TODO: karten Mischen und Spielern zuweisen, in 2 ViererBlöcken für Klopf-Mechanismen
+    return karten.copy()
     
-def markTrumpf(cards,trumpf,special):
-    if special == 'none':
-        #Farbspiel
-        #set Trumpf
+def setzeTrumpfUndSpiel(cards,trumpf,spielArt):
+    if spielArt == 'Farbspiel' or spielArt == 'Solo':
+        #Farbspiel oder solo
+        if spielArt == 'Farbspiel':
+            trumpf = 'Herz'
+        #definiere und markiere Trumpfkarten
         cards['Trumpf'] = False
         cards.loc[cards['Farbe'] == trumpf,'Trumpf'] = True
         cards.loc[cards['Name'] == 'Ober','Trumpf'] = True
         cards.loc[cards['Name'] == 'Unter','Trumpf'] = True
-        #define order
-        cards['Order'] = 0
-        cards.loc[cards['Name'] == 'Ober']
+        #definiere rangfolge der Karten
+
 
     print(cards)
     return cards
 
 if __name__ == "__main__":
-    cards = createCards()
-    gameSet = markTrumpf(cards,'Eiche','none')
+    basisKarten = erstelleBlatt('lang')
+    spielKarten = verteileKarten(basisKarten)
+    #warte auf  Spielentscheidung
+    spielKarten = setzeTrumpfUndSpiel(spielKarten,'Eiche','Farbspiel')
